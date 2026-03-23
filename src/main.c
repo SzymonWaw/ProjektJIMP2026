9908d7cd1c06eaaa083b2853448262c485a4ca3d
Kamirox1805 <kamirox2006@gmail.com>
2026-03-23 19:01:51 +0100

    Zmiana struktór na docelowe i przeniesienie plików z folderu build do src bo w złym zacząłem pisać

--- src/main.c
+++ src/main.c
@@ -1,5 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char **argv){
-    
+typedef struct{
+	double x;
+	double y;
+} Coordinates;
+typedef struct{
+	int id;
+	Coordinates cords;
+} Point;
+
+typedef struct{
+	char *name;
+	double weight;
+	Point A;
+	Point B;
+} Edge;
+
+int main(int argc, char **argv)
+{
+	FILE *file = fopen(argv[1], "r");
+	if (!file)
+	{
+		perror("Usage: program <filename>");
+		return EXIT_FAILURE;
+	}
+
+	while(!feof(file))
+	{
+		
+	}
 }
